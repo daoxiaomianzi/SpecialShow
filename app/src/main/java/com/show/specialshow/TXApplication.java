c3ec8c0e090620ae6e849a4e36c9f79239d8c39a
daoxiaomianzi <1940393554@qq.com>
2016-12-13 15:15:42 +0800

    配置极光推送

--- app/src/main/java/com/show/specialshow/TXApplication.java
+++ app/src/main/java/com/show/specialshow/TXApplication.java
@@ -45,6 +45,8 @@ import com.umeng.socialize.PlatformConfig;
 
 import org.json.JSONObject;
 
+import cn.jpush.android.api.JPushInterface;
+
 public class TXApplication extends MultiDexApplication {
 	public static int WINDOW_WIDTH = 0;
 	public static int WINDOW_HEIGHT = 0;
@@ -79,6 +81,8 @@ public class TXApplication extends MultiDexApplication {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
+		JPushInterface.init(this);     		// 初始化 JPush
 		initData();
 		DisplayImageOptions defaultOptions = new DisplayImageOptions
 				.Builder()
