e9c20b21f2553eae11455a3e1fbde2eb5f785fdf
daoxiaomianzi <1940393554@qq.com>
2016-12-28 20:27:33 +0800

    修复bug

--- app/src/main/java/com/show/specialshow/model/CircleDynamicList.java
+++ app/src/main/java/com/show/specialshow/model/CircleDynamicList.java
@@ -6,23 +6,28 @@ import com.alibaba.fastjson.JSON;
 
 public class CircleDynamicList extends BaseList {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -1265606923510463L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = -1265606923510463L;
 
-	private List<CircleDynamicItem> list;
+    private List<CircleDynamicItem> list;
 
-	public List<CircleDynamicItem> getList() {
-		return list;
-	}
+    public List<CircleDynamicItem> getList() {
+        return list;
+    }
 
-	public void setList(List<CircleDynamicItem> list) {
-		this.list = list;
-	}
+    public void setList(List<CircleDynamicItem> list) {
+        this.list = list;
+    }
 
-	public static CircleDynamicList parse(String result) {
-		return JSON.parseObject(result, CircleDynamicList.class);
-	}
+    public static CircleDynamicList parse(String result) {
+        try {
+            return JSON.parseObject(result, CircleDynamicList.class);
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
 
 }
